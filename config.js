aef6d2bdc7756fb70752b372299e93deff0c7f75
OveeKawre <74299130+OveeKawre@users.noreply.github.com>
2023-04-22 11:46:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDunhLuFDsLvPejjGsc2EMxsPBbzbP-hco",
+  authDomain: "spectagram-app-39057.firebaseapp.com",
+  databaseURL: "https://spectagram-app-39057-default-rtdb.firebaseio.com",
+  projectId: "spectagram-app-39057",
+  storageBucket: "spectagram-app-39057.appspot.com",
+  messagingSenderId: "878622931705",
+  appId: "1:878622931705:web:c42867233c88e0560d4028"
+  };
+  
